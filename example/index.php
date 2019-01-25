56ddfefcb524428002abf77f407f2998413bb07c
Aleksandar Zivanovic <coapsyfactor@gmail.com>
2019-01-25 15:11:16 +0100

    Added better example

--- example/index.php
+++ example/index.php
@@ -6,4 +6,18 @@ require_once __DIR__ . '/../vendor/autoload.php';
 
 $T = new Translator('en_US', __DIR__ . '/translations');
 
-echo $T->translate('USER_CREATED', [':username:' => 'AwesomeName']);
+// Basic translate
+echo $T->translate('WELCOME') . "\n";
+
+// Translate with replacable placeholders
+// NOTE: not defining section nor strict only - script will look for first translation with id "CREATED"
+echo $T->translate('CREATED', [':username:' => 'Test User']) . "\n";
+
+// Translate with replacable placeholders
+// NOTE: defining section without using strict only, script will look for "CREATED" in section "BLOG_POST"
+echo $T->translate('CREATED', [':username:' => 'BestUser', ':title:' => 'Post Title'], 'BLOG_POST') . "\n";
+
+// Translate with replacable placeholders
+// NOTE: defining section while using strict only - script wil throw an exception if requested identifier doesn't exist
+// in provided section
+echo $T->translate('REMOVED', [':username:' => 'BestUser', ':title:' => 'Its a title'], 'USER', true);
